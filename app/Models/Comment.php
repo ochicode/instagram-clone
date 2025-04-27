143e488ea25a0045313ad3fc92eee850a0c369cc
Benard <benard518@gmail.com>
2025-04-27 12:43:34 +0300

    adding models

---
+++ app/Models/Comment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use MongoDB\Laravel\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $connection = 'mongodb';
+    protected $table = 'comments';
+
+    protected $fillable = [
+        'comment_id',
+        'user_id',
+        'post_id',
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
